1bb9cf506585231fd8ad44158b12a8cbb1a38f5c
Adil Enver <Adiljan.anwar@gmail.com>
2023-02-13 14:53:04 +0300

    DefineXFinalProject

---
+++ DefineXPracticumFinalCase/ReadMe.md
@@ -0,0 +1,3 @@
+![](ScreenShot/defineX.png)
+
+---
---
+++ DefineXPracticumFinalCase/src/test/java/com/adilenver/DefineXPracticumFinalCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adilenver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefineXPracticumFinalCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
